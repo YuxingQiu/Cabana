226352526bb0c296e2e0185d58cba32619d6e585
YuxingQiu <yuxqiu@gmail.com>
2021-05-26 01:15:03 -0700

    add performance test for sparsemap

---
+++ benchmark/Cajita_SparseMapPerformance.cpp
@@ -0,0 +1,262 @@
+
+/****************************************************************************
+ * Copyright (c) 2018-2021 by the Cabana authors                            *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the Cabana library. Cabana is distributed under a   *
+ * BSD 3-clause license. For the licensing terms see the LICENSE file in    *
+ * the top-level directory.                                                 *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#include "Cabana_BenchmarkUtils.hpp"
+
+#include <Cabana_Core.hpp>
+#include <Cajita_SparseIndexSpace.hpp>
+
+#include <Kokkos_Core.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace Cajita;
+//---------------------------------------------------------------------------//
+// Performance test.
+template <class Device>
+void performanceTest( std::ostream& stream, const std::string& test_prefix )
+{
+    // Domain size setup
+    std::array<float, 3> global_low_corner = { 0.0, 0.0, 0.0 };
+    std::array<float, 3> global_high_corner = { 1.0, 1.0, 1.0 };
+    constexpr int cell_num_per_tile_dim = 4;
+    constexpr int cell_bits_per_tile_dim = 2;
+
+    // Declare the total number of particles to be inserted.
+    std::vector<int> num_particles = { 1000, 10000, 100000, 1000000 };
+    int num_particles_size = num_particles.size();
+
+    // Declare the size (cell nums) of the domain
+    std::vector<int> num_cells_per_dim = { 32, 64, 128, 256, 512, 1024 };
+    int num_cells_per_dim_size = num_cells_per_dim.size();
+
+    // Generate a random set of particles in domain [0.0, 1.0]
+    Kokkos::View<float* [3], Device> poses(
+        Kokkos::ViewAllocateWithoutInitializing( "particle_poses" ),
+        num_particles.back() );
+    Kokkos::View<float* [3], Kokkos::HostSpace> host_poses(
+        Kokkos::ViewAllocateWithoutInitializing( "host_particle_poses" ),
+        num_particles.back() );
+    std::minstd_rand0 generator( 3439203991 );
+    float generator_range = generator.max() - generator.min();
+    for ( int n = 0; n < num_particles.back(); ++n )
+    {
+        for ( int d = 0; d < 3; ++d )
+            host_poses( n, d ) =
+                static_cast<float>( ( generator() - generator.min() ) ) /
+                generator_range;
+    }
+    Kokkos::deep_copy( poses, host_poses );
+
+    // Number of runs in the test loops.
+    int num_run = 10;
+
+    // Some helper views
+    int max_num_tiles_per_dim =
+        num_cells_per_dim.back() / cell_num_per_tile_dim;
+    Kokkos::View<uint64_t***, Device> tile_ids(
+        Kokkos::ViewAllocateWithoutInitializing( "tile_ids" ),
+        max_num_tiles_per_dim, max_num_tiles_per_dim, max_num_tiles_per_dim );
+    Kokkos::View<bool***, Device> is_tile_valid(
+        Kokkos::ViewAllocateWithoutInitializing( "is_tile_valid" ),
+        max_num_tiles_per_dim, max_num_tiles_per_dim, max_num_tiles_per_dim );
+
+    // Loop over different size of the domain
+    for ( int c = 0; c < num_cells_per_dim_size; ++c )
+    {
+        std::array<int, 3> global_num_cell = {
+            num_cells_per_dim[c], num_cells_per_dim[c], num_cells_per_dim[c] };
+        auto global_mesh = createSparseGlobalMesh(
+            global_low_corner, global_high_corner, global_num_cell );
+        float cell_size = 1.0 / num_cells_per_dim[c];
+        int num_tiles_per_dim = num_cells_per_dim[c] >> cell_bits_per_tile_dim;
+
+        // create sparse map
+        int pre_alloc_size = num_cells_per_dim[c] * num_cells_per_dim[c];
+        auto sis = createSparseMap<typename Device::execution_space>(
+            global_mesh, pre_alloc_size );
+
+        // Create insertion timers
+        std::stringstream insert_time_name;
+        insert_time_name << test_prefix << "cell_insert_"
+                         << "domain_size_" << num_cells_per_dim[c];
+        Cabana::Benchmark::Timer insert_timer( insert_time_name.str(),
+                                               num_particles_size );
+
+        std::stringstream query_time_name;
+        query_time_name << test_prefix << "cell_query_"
+                        << "domain_size_" << num_cells_per_dim[c];
+        Cabana::Benchmark::Timer query_timer( query_time_name.str(),
+                                              num_particles_size );
+
+        std::stringstream valid_tile_ijk_time_name;
+        valid_tile_ijk_time_name << test_prefix << "get_valid_tile_ijk_"
+                                 << "domain_size_" << num_cells_per_dim[c];
+        Cabana::Benchmark::Timer valid_tile_ijk_timer(
+            valid_tile_ijk_time_name.str(), num_particles_size );
+
+        for ( int p = 0; p < num_particles_size; ++p )
+        {
+            for ( int t = 0; t < num_run; ++t )
+            {
+                // init helper views
+                Kokkos::deep_copy( tile_ids, 0 );
+                Kokkos::deep_copy( is_tile_valid, 0 );
+                Kokkos::parallel_for(
+                    "label_valid_cells",
+                    Kokkos::RangePolicy<typename Device::execution_space>(
+                        0, num_particles[p] ),
+                    KOKKOS_LAMBDA( const int par_id ) {
+                        int ti = static_cast<int>( poses( par_id, 0 ) /
+                                                   cell_size ) >>
+                                 cell_bits_per_tile_dim;
+                        int tj = static_cast<int>( poses( par_id, 1 ) /
+                                                   cell_size ) >>
+                                 cell_bits_per_tile_dim;
+                        int tk = static_cast<int>( poses( par_id, 2 ) /
+                                                   cell_size ) >>
+                                 cell_bits_per_tile_dim;
+                        is_tile_valid( ti, tj, tk ) = true;
+                    } );
+
+                // insertion
+                insert_timer.start( p );
+                Kokkos::parallel_for(
+                    "insert_cells_to_sparse_map",
+                    Kokkos::RangePolicy<typename Device::execution_space>(
+                        0, num_particles[p] ),
+                    KOKKOS_LAMBDA( const int par_id ) {
+                        int ci =
+                            static_cast<int>( poses( par_id, 0 ) / cell_size );
+                        int cj =
+                            static_cast<int>( poses( par_id, 1 ) / cell_size );
+                        int ck =
+                            static_cast<int>( poses( par_id, 2 ) / cell_size );
+                        sis.insertCell( ci, cj, ck );
+                    } );
+                insert_timer.stop( p );
+
+                // query
+                query_timer.start( p );
+                Kokkos::parallel_for(
+                    "query_cell_ids_from_cell_ijk",
+                    Kokkos::RangePolicy<typename Device::execution_space>(
+                        0, num_particles[p] ),
+                    KOKKOS_LAMBDA( const int par_id ) {
+                        int ci =
+                            static_cast<int>( poses( par_id, 0 ) / cell_size );
+                        int cj =
+                            static_cast<int>( poses( par_id, 1 ) / cell_size );
+                        int ck =
+                            static_cast<int>( poses( par_id, 2 ) / cell_size );
+                        tile_ids( ci >> cell_bits_per_tile_dim,
+                                  cj >> cell_bits_per_tile_dim,
+                                  ck >> cell_bits_per_tile_dim ) =
+                            sis.queryTile( ci, cj, ck );
+                    } );
+                query_timer.stop( p );
+
+                // get valid tile_ijk
+                valid_tile_ijk_timer.start( p );
+                Kokkos::parallel_for(
+                    "compute_tile_ijk_from_tile_id",
+                    Kokkos::RangePolicy<typename Device::execution_space>(
+                        0, sis.capacity() ),
+                    KOKKOS_LAMBDA( const int index ) {
+                        if ( sis.valid_at( index ) )
+                        {
+                            auto tileKey = sis.key_at( index );
+                            auto tileId = sis.value_at( index );
+                            int qti, qtj, qtk;
+                            sis.key2ijk( tileKey, qti, qtj, qtk );
+                            if ( !is_tile_valid( qti, qtj, qtk ) )
+                            {
+                                printf( "error occured [Not valid]: (ti, tj, "
+                                        "tk) = (%d, %d, "
+                                        "%d)",
+                                        qti, qtj, qtk );
+                            }
+                            else if ( tileId != tile_ids( qti, qtj, qtk ) )
+                            {
+                                printf(
+                                    "error occured: (ti, tj, tk) = (%d, %d, "
+                                    "%d), tileKey = %lu, tileId = %lu, queried "
+                                    "ID = %lu\n",
+                                    qti, qtj, qtk, tileKey, tileId,
+                                    tile_ids( qti, qtj, qtk ) );
+                            }
+                        }
+                    } );
+                valid_tile_ijk_timer.stop( p );
+            }
+        }
+
+        // Output results
+        outputResults( stream, "particle_num", num_particles, insert_timer );
+        outputResults( stream, "particle_num", num_particles, query_timer );
+        outputResults( stream, "particle_num", num_particles,
+                       valid_tile_ijk_timer );
+    }
+}
+
+//---------------------------------------------------------------------------//
+// main
+int main( int argc, char* argv[] )
+{
+    // Initialize environment
+    Kokkos::initialize( argc, argv );
+
+    // Check arguments.
+    if ( argc < 2 )
+        throw std::runtime_error( "Incorrect number of arguments. \n \
+             First argument - file name for output \n \
+             \n \
+             Example: \n \
+             $/: ./SparseMapPerformance test_results.txt\n" );
+
+    // Get the name of the output file.
+    std::string filename = argv[1];
+
+    // Open the output file on rank 0.
+    std::fstream file;
+    file.open( filename, std::fstream::out );
+
+    // Run the tests.
+#ifdef KOKKOS_ENABLE_SERIAL
+    using SerialDevice = Kokkos::Device<Kokkos::Serial, Kokkos::HostSpace>;
+    performanceTest<SerialDevice>( file, "serial_" );
+#endif
+
+#ifdef KOKKOS_ENABLE_OPENMP
+    using OpenMPDevice = Kokkos::Device<Kokkos::OpenMP, Kokkos::HostSpace>;
+    performanceTest<OpenMPDevice>( file, "openmp_" );
+#endif
+
+#ifdef KOKKOS_ENABLE_CUDA
+    using CudaDevice = Kokkos::Device<Kokkos::Cuda, Kokkos::CudaSpace>;
+    // using CudaUVMDevice = Kokkos::Device<Kokkos::Cuda, Kokkos::CudaUVMSpace>;
+    performanceTest<CudaDevice>( file, "cuda_" );
+    // performanceTest<CudaDevice>( file, "cudauvm_" );
+#endif
+
+    // Close the output file on rank 0.
+    file.close();
+
+    // Finalize
+    Kokkos::finalize();
+    return 0;
+}
